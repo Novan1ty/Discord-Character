e5b38ec1282c9a0f60238058ea7711a974ffdeb0
Novanity <75135053+Novan1ty@users.noreply.github.com>
2021-10-05 16:49:57 +0800

    V1.8 ~ 10/5/21; October 5, 2021

---
+++ Example/Rune.js
@@ -0,0 +1,61 @@
+/*
+Copy and Paste this if running this is necessary.
+~ 10/4/21; October 4, 2021
+*/
+
+const { Character_Set, Character } = require('../Character-Class.js')
+
+module.exports = client => {
+    client.on('messageCreate', async message => {
+        if (message.author.bot) return;
+
+        const Name = 'Rune'
+        const Runes_Avatar = 'https://cdn.discordapp.com/avatars/781224758355820555/de3ad39b494018d9e42336610d7691ef.jpg?size=4096'
+        
+        const Path = './Storages/Characters/Rune.json'
+        
+        // Character Set ~ 10/2/21; October 2, 2021
+        /*
+        {
+            const Rune = new Character_Set(client, message, Name, Runes_Avatar, Path)
+
+            const Main_Responses = [
+                'What?', 'Seriously.', 'Shut up.',
+                'No?', 'Yeah?', 'Hm, no.', 'What.',
+                'Uhhh', 'Idk.'
+            ]
+            const Status_Responses = [
+                'I\'m doing good, how about you? ' + message.author.username  + '.',
+            ]
+
+            Rune.Set_Main(Main_Responses)
+            Rune.Set_Status(Status_Responses)
+
+            return await Rune.Chat()
+        }
+        */
+        
+        // Character ~ 10/4/21; October 4, 2021
+
+        const Rune = new Character(client, message, Name, Runes_Avatar, Path)
+
+        const Main_Responses = [
+            'What?', 'Seriously.', 'Shut up.',
+            'No?', 'Yeah?', 'Hm, no.', 'What.',
+            'Uhhh', 'Idk.'
+        ]
+        Rune.Set_Main(Main_Responses)
+
+        const Mitch = [ 'Mitch' ]
+        const Mitch_Responses = [
+            'Oh no-', 'Oh no.', 'Uh oh-', 'Uh oh.'
+        ]
+        
+        const A = [ 'A', 'EA' ]
+        const A_Responses = [ 'A.', 'a.', 'EA.', 'ea.' ]
+        Rune.Add_Response(Mitch, Mitch_Responses, 2)
+        Rune.Add_Response(A, A_Responses, 1)
+
+        return await Rune.Chat()
+    })
+}
---
+++ Help.md
@@ -0,0 +1,4 @@
+# Note
+The Classes does not have examples, so check it [here](http://npmjs.org/)
+
+Thanks for reading this, and wasting your time... maybe.
